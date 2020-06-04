884065ee2c3b92cbaf9e1b13ffb7e52c9e4a6a34
youto614 <50744294+youto614@users.noreply.github.com>
2020-06-04 13:39:29 +0800

    Update Main.java

--- SparkSQL/src/application/Main.java
+++ SparkSQL/src/application/Main.java
@@ -1,16 +1,39 @@
 package application;
 
+import java.sql.SQLException;
+
 import controller.Manager;
 import controller.StartedControl;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import test.GenTestData;
 import view.StartedWin;
+import model.*;
 
 
 public class Main extends Application {
+
+	  String sqldriverClassName="org.apache.mysql.jdbc.mysqlDriver";
+	  String conname="bigdata";
+	  String sqlurl="bigdata31.depts.bingosoft.net";
+	  String sqlpost="22231";	
+	  String sqlusername="user08";
+	  String sqlpassword="pass@bingo8";
+	  String sqldb="user08_db";
+
 	@Override
+	
 	public void start(Stage primaryStage) {
+		//获取连接
+		SparkSQLConConfig config=new SparkSQLConConfig(conname,sqlurl,sqlpost,sqlusername,sqlpassword);
+		SparkSQLService serv=new SparkSQLService(config,sqldb);//至少需要连接一个数据库
+		try {
+			serv.getSparkSQLService();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		StartedWin startedWin = new StartedWin();
 		StartedControl startedControl = new StartedControl(startedWin);
 		
@@ -21,8 +44,8 @@ public class Main extends Application {
 	    primaryStage.show();
 	    
 	    //test code
-	    startedControl.addSQLConInfo(GenTestData.genSQLConInfo());
-	    startedControl.addSQLConInfo(GenTestData.genSQLConInfo());
+	    startedControl.addSQLConInfo(GenTestData.genSQLConInfo(serv));
+	    //startedControl.addSQLConInfo(GenTestData.genSQLConInfo());
 	    startedControl.addSQLResult(GenTestData.genSQLResult());
 	    startedControl.test();
 	}
